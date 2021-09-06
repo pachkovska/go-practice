60e7cfa09c1bab99c430a66fc2cdb29d614962ab
pachkovska <pachkovska@gmail.com>
2021-09-05 19:48:24 -0700

    add running sum

---
+++ leetcodePractice/runningSumOf1dArray.go
@@ -0,0 +1,12 @@
+package leetcodePractice
+
+func runningSum(nums []int) []int {
+	result := make([]int, len(nums))
+	result[0] = nums[0]
+
+	for i := 1; i < len(nums) ; i++ {
+		result[i] = result[i-1] + nums[i]
+	}
+
+	return result
+}
